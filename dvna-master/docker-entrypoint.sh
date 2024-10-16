14df921175df253183053d6808d3bb801ab73c73
Christophersalem0 <90207292+Christophersalem0@users.noreply.github.com>
2024-10-15 21:17:39 -0400

    fix up

--- dvna-master/docker-entrypoint.sh
+++ dvna-master/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-chmod +x /app/wait-for-it.sh
+chmod +x /dvna/wait-for-it.sh
 
-/bin/bash /app/wait-for-it.sh $MYSQL_HOST:$MYSQL_PORT -t 300 -- npm start
+/bin/bash /dvna/wait-for-it.sh $MYSQL_HOST:$MYSQL_PORT -t 300 -- npm start
